a5eac8aedb284d760434ca1acf1cf4cae73e76a9
devbxtzz <batztonnie@gmail.com>
2023-03-02 16:03:09 +0300

    Updated social profile links

--- components/icons/LinkedInProfile.js
+++ components/icons/LinkedInProfile.js
@@ -4,7 +4,7 @@ const LinkedInProfile = ({ marginBottom }) => {
   return (
     <div className={`w-8 h-8 ${marginBottom}`}>
       <a
-        href="http://www.linkedin.com/in/daniel-cranney"
+        href="https://www.linkedin.com/in/batya-tonny-108a92234/"
         target="_blank"
         rel="noreferrer"
       >
