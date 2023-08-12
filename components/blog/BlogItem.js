ebd2ff1e71f8a13256c6e7e072d2153c67127d62
devbxtzz <batztonnie@gmail.com>
2023-08-12 20:59:39 +0300

    updated blog section

--- components/blog/BlogItem.js
+++ components/blog/BlogItem.js
@@ -35,27 +35,7 @@ export default function BlogItem({ post }) {
         <p className="mb-0">{post.brief.substr(0, 150)}...</p>
         <p className="btn-xs btn-brand self-start mt-3">Read more</p>
       </div>
-      <div className="w-full h-full absolute dark:bg-dark/80 bg-white/80 opacity-0 group-hover:opacity-100 top-0 left-0 z-50 transition-all duration-150 ease-in-out">
-        <div className="flex w-full h-full items-center justify-center">
-          <div className="btn-sm btn-brand rounded-sm flex items-center gap-x-1.5">
-            <p className="mb-0 dark:text-white text-white">Open</p>
-            <svg
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-              ></path>
-            </svg>
-          </div>
-        </div>
-      </div>
+      
     </div>
   );
 }
