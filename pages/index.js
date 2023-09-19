3aab98128d3bd2d842aedb7c46963951d2bc440b
Tonny Batya <84370752+devbxtzz@users.noreply.github.com>
2023-09-19 02:47:16 -0700

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -489,6 +489,16 @@ export default function Home({ publications }) {
                 <h1 className="m-12 text-2xl sm:text-6xl dark:text-white text-dark">
                   TONNY BATYA
                 </h1>
+
+                <div className="relative h-48 w-48 md:h-96 md:w-96"> //"relative" is required; change height and width if needed
+                    <Image
+                        src='/images/logo.png'
+                        alt="Logo"
+                        layout="fill" // required
+                        objectFit="cover" // change as you like
+                        className="rounded-full" // you can use other classes here too
+                    />
+                </div>
                 
                 <p className="m-8 text-center text-xl sm:text-2xl dark:text-light text-mid">
                   Software Developer, Blockchain and AI Enthusiast, Writer and Founder at the Forefront of Emerging Technologies. Fusing Proficiency in Coding with a Passion for Blockchain and AI to Engineer and Build Extraordinary Online Experiences
